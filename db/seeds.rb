f9c8d411a36993ae43b73fa54bcd5367b8da3473
ryota tanabe <teni0abe@gmail.com>
2020-05-18 15:32:50 +0900

    [add]seeds.rbにテストデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ToDo.create(title: 'トーナメントに出る')
+ToDo.create(title: 'snowieで300ハンドやる')
