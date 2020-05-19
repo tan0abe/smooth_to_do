b9f6e0c314fedc3d426ecfd11e2b2327c4a113d3
ryota tanabe <teni0abe@gmail.com>
2020-05-19 17:06:10 +0900

    [add]ToDoを編集できるようにupdateアクションを作成

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -20,6 +20,16 @@ class ToDosController < ApplicationController
     @to_do = ToDo.find_by(id: params[:id])
   end
 
+  def update
+    @to_do = ToDo.find_by(id: params[:id])
+    if @to_do.update(to_do_params)
+      flash[:success] = "タスクを更新しました!"
+      redirect_to to_dos_url
+    else
+      render :edit
+    end
+  end
+
   private
 
   def to_do_params
