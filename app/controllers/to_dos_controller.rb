6fc58e4f818eca6779cba224e014e1d1a3355094
ryota tanabe <teni0abe@gmail.com>
2020-05-19 16:46:03 +0900

    [add]createアクションを作成

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -6,4 +6,19 @@ class ToDosController < ApplicationController
   def new
     @to_do = ToDo.new
   end
+
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save
+      redirect_to to_dos_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def to_do_params
+    params.require(:to_do).permit(:title)
+  end
 end
