08375a63474393be4a775a3f000dcd295054e77e
ryota tanabe <teni0abe@gmail.com>
2020-05-31 16:37:17 +0900

    [update]時刻のデフォルトフォーマットを変更(lメソッドを使用していたがvue側で上手く反映されなかったため。参照:https://00m.in/Ac90F)

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module SmoothToDo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
 
